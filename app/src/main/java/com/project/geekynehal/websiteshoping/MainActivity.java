cba34cb0e45f6e21c3adcd2c39d19685a43f5579
Nehal Kumar Singh <nehal@localhost.localdomain>
2019-03-02 02:40:10 +0530

    Login and signup initialized

---
+++ app/src/main/java/com/project/geekynehal/websiteshoping/MainActivity.java
@@ -0,0 +1,14 @@
+ package com.project.geekynehal.websiteshoping;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
